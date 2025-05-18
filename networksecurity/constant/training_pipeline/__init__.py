38b002073d1e1ec05e5ec842dfd7aa8a9ec77135
Hardik Kumar <Hardikkumar885@gmail.com>
2025-05-18 14:56:57 +0530

    Data Validation Completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -12,6 +12,8 @@ FILE_NAME:str="network_data\Phishing_Websites_Data.csv"
 TRAIN_DATA_FILE:str="train.csv"
 TEST_DATA_FILE:str="test.csv"
 
+SCHEMA_FILE_PATH:str=os.path.join("data_schema","schema.yaml")
+
 '''----DATA INGESTION CONSTANTS----'''
 DATA_INGESTION_COLLECTION_NAME:str="NETWORKDATA"
 DATA_INGESTION_DATABASE_NAME :str="HARDIK"
@@ -19,3 +21,11 @@ DATA_INGESTION_DIR_NAME :str="DataIngestion"
 DATA_INGESTION_FEATURE_STORE_DIR :str="feature_store"
 DATA_INGESTION_INGESTED_DIR :str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO :float=0.2
+
+'''----DATA VALIDATION CONSTANTS----'''
+DATA_VALIDATION_DIR_NAME:str="data_validation"
+DATA_VALIDATION_VALID_DIR:str="validated"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+
---
+++ networksecurity/util/main_utils/utils.py
@@ -0,0 +1,26 @@
+import yaml
+import dill
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import os,sys
+import numpy as np
+import pickle
+
+def read_yaml_file(filepath:str)->dict:
+    try:
+        with open(filepath,'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def write_yaml_file(filepath:str,content:object,replace:bool=False)->None:
+    try:
+        if replace and os.path.exists(filepath):
+            os.remove(filepath)
+        os.makedirs(os.path.dirname(filepath),exist_ok=True)
+        with open(filepath,"w") as yaml_file:
+            yaml.dump(content,yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+
